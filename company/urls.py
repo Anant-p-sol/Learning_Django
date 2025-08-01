750ee7f3aedb703e4ebd0b03d392b11d926af5b6
Anant Kumar <adwayaanant@gmail.com>
2025-08-02 01:24:47 +0530

    add buitin index.html

--- company/urls.py
+++ company/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app.views import index, aboutus # Assuming 'index' is the name of your Django view
+from app.views import index # Assuming 'index' is the name of your Django view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index,name="home"),
-    path('aboutus/',aboutus, name="aboutus"),
+    
 
 ]
