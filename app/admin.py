863b6dcc32ae1da8f02b15b04b69aba6468ad310
Anant Kumar <adwayaanant@gmail.com>
2025-08-05 09:53:41 +0530

    model creation

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from app.models import GeneralInfo
+
+@admin.register(GeneralInfo)
+class GeneralInfoAdmin(admin.ModelAdmin):
+    pass  # You can customize the admin interface here if needed
