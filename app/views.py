864dc443d7e6e7db98d2793b0aa9904abb250e92
Anant Kumar <adwayaanant@gmail.com>
2025-08-05 11:21:09 +0530

    ORM Quaries to read

--- app/views.py
+++ app/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
+from app.models import GeneralInfo
+from django.db import connection
 
+def write_sql_quarys_to_file(file_path):
+    with open('sql_queries.txt', 'w') as file:
+        queries = connection.queries
+        for query in queries:
+            sql = query['sql']
+            file.write(f"{sql}\n")
 
 # Create your views here.
 
-def index(request):
+def index(request): 
+    #read                                        # ORM (Object-Relational Mapping) to fetch data from the database
+    all_records = GeneralInfo.objects.all()      # ORM is a programing languagetechnique that allows devlopers to interact with 
+    print(all_records)                                # the database using the programming language's syntax instead of SQL queries.
+
+
+    file_path = 'sql_queries.txt'
+    write_sql_quarys_to_file(file_path)  # Write SQL queries to a file
+
     context = {}
     return render(request,"index.html", context)
 
-def aboutus(request):
-    context = {}
-    return render(request,"aboutus.html", context)
