acf39b6706e9c33f3e3dda0c61ed4cfaa73d93bd
Anant Kumar <adwayaanant@gmail.com>
2025-08-05 10:01:29 +0530

    customize the admin interface(disply, add, delete and update)

--- app/admin.py
+++ app/admin.py
@@ -5,4 +5,18 @@ from app.models import GeneralInfo
 
 @admin.register(GeneralInfo)
 class GeneralInfoAdmin(admin.ModelAdmin):
-    pass  # You can customize the admin interface here if needed
+    
+    list_display = [
+        'company_name', 'location', 'email', 'phone', 'open_hours',
+    ]  # You can customize the admin interface here if needed
+
+    # show to diable add permission
+    def has_add_permission(self, request, obj=None):
+        return False
+    
+    # show to diable change permission
+    def has_change_permission(self, request, obj=None):
+        return False
+    
+    def has_delete_permission(self, request, obj=None):
+        return False                
